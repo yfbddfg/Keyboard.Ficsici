51c5d0d691ad5a9c2cbdce044554180b599f3900
yfbddfg <nastenast32@gmail.com>
2025-01-12 16:34:03 +0300

    Add files via upload

---
+++ write_count_fine.py
@@ -0,0 +1,16 @@
+import check_fine as c_fine
+
+
+def write_count_fine(strs, fine_dict_qwerty, fine_dict_zubachew):
+
+    for ch in strs:
+        presseddict1 = c_fine.check_fine_zubachew(ch)
+        presseddict2 = c_fine.check_fine_qwerty(ch)
+
+        for fingerkey in presseddict1:
+            fine_dict_zubachew[fingerkey] += presseddict1[fingerkey]
+
+        for fingerkey in presseddict2:
+            fine_dict_qwerty[fingerkey] += presseddict2[fingerkey]
+    return fine_dict_qwerty, fine_dict_zubachew
+
